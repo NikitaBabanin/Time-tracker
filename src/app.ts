e9527745be7858738f29cf79110a952cca14b234
NikitaBabanin <postmanngmilus@gmail.com>
2022-01-15 06:29:45 +0200

    Added logger.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
+import {LoggerService} from './logger/logger.service'
 
 export class App {
     app: Express;
     port: number;
-    server: Server
+    server: Server;
+    logger: LoggerService;
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
-        this.port = 8001;
+        this.port = 8002;
+        this.logger = logger;
     }
 
     public async init(){
-        this.server = this.app.listen(this.port,() => console.log('server is running'));
+        this.server = this.app.listen(this.port);
+        this.logger.log(`Server is running on port ${this.port} `)
     }
 }
